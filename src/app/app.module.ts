cd7ea04c038709164e2087be93dec8487e47b9d8
nycolemendonca-acs <nycolemendonca.acs@gmail.com>
2023-06-12 15:00:21 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext'; 
+import { ButtonModule } from 'primeng/button';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgModule,
+    TabViewModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
